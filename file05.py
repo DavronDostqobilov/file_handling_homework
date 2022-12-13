56ee776bbcf3df2f23fd580b08632b88d068e5f2
DavronDostqobilov <dostqobilovdavron885@gmail.com>
2022-12-13 21:31:44 +0500

    file05

--- file05.py
+++ file05.py
@@ -1,4 +1,4 @@
-def main(data:str):
+def main(data):
     """
     The data is from the file. Find the number of digital and str(non-digital) data and return as list type.
     Args:
@@ -6,5 +6,18 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    list1=[]
+    n=0
+    m=0
+    for i in range(len(data)):
+        if data[i].isdigit():
+            m+=1
+        else:
+            n+=1
+    list1.append(m)
+    list1.append(n)
+    return list1
+f=open('txt_file/data05.txt')
+a=f.read()
+print(main(a)) 
 # Read data from file
