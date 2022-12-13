a55dff30aed946a9541124d2575ae54c534b6ccf
DavronDostqobilov <dostqobilovdavron885@gmail.com>
2022-12-14 00:00:25 +0500

    file09

--- file09.py
+++ file09.py
@@ -6,5 +6,13 @@ def main(data:str):
     Returns:
         int: return answer
     """
-
+    mx=9
+    for i in range(len(data)):
+        if data[i].isdigit():
+            if mx>=int(data[i]):
+                mx=int(data[i])
+    return mx
+f=open('txt_file/data09.txt')
+a=f.read()
+print(main(a))
 # Read data from file
