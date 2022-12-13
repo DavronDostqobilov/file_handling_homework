4c7f04447a9a516065f107d45ae6f24107e27ceb
DavronDostqobilov <dostqobilovdavron885@gmail.com>
2022-12-13 22:59:38 +0500

    file07

--- file07.py
+++ file07.py
@@ -6,5 +6,14 @@ def main(data:str):
     Returns:
         int: return answer
     """
-    
+    list1=[]
+    sum=0
+    for i in range(len(data)):
+        if data[i].isdigit():
+            sum+=int(data[i])
+    list1.append(sum)
+    return list1
+f=open('txt_file/data07.txt')
+a=f.read()
+print(main(a))    
 # Read data from file
