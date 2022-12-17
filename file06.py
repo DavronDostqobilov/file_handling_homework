6b575c6bc1085408dde13eb9fd986bf91e8a2695
DavronDostqobilov <dostqobilovdavron885@gmail.com>
2022-12-17 14:12:22 +0500

    file06

--- file06.py
+++ file06.py
@@ -1,4 +1,4 @@
-def main(data:str):
+def main(data):
     """
     The data is from the file. Find the each row length and return as list type.
     Args:
@@ -6,5 +6,13 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
-# Read data from file
+    list1=data.split()
+    k=len(list1[0])
+    for i in list1:
+        if k<len(i):
+            k=len(i)
+    return k
+f=open('txt_file/data06.txt')
+a=f.read()
+print(main(a))
+
