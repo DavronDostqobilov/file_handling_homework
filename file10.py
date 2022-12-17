2289a8d6d2131eac549bd162e8935f50e837b56e
DavronDostqobilov <dostqobilovdavron885@gmail.com>
2022-12-17 14:15:14 +0500

    file10

--- file10.py
+++ file10.py
@@ -1,4 +1,4 @@
-def main(data:str):
+def main(data):
     """
     The data is from the file. Find the each row length and return the largest row.
     Args:
@@ -6,5 +6,13 @@ def main(data:str):
     Returns:
         int: return answer
     """
-
+    list1=data.split()
+    k=len(list1[0])
+    for i in list1:
+        if k<len(i):
+            k=len(i)
+    return k
+f=open('txt_file/data06.txt')
+a=f.read()
+print(main(a))
 # Read data from file
